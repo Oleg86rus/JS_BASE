c2e17932aaa26eb8390b9c78b83b6da9a87b96ec
oleg086rus <o.pototskiy@yandex.ru>
2022-01-10 14:30:26 +0500

    Syntax

---
+++ first_project/Syntax/car.js
@@ -0,0 +1,28 @@
+const carName = 'BMW';
+let maxCarSpeed = 250;
+let carOwner = 'Олег';
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = 240;
+carOwner = 'София';
+alert(maxCarSpeed);
+alert(carOwner);
+console.log('У машины изменилась максимальная скорость', maxCarSpeed);
+console.log('У машины изменился владелец', carOwner);
+
+maxCarSpeed = 230;
+carOwner = 'Виктор';
+alert(maxCarSpeed);
+alert(carOwner);
+console.log('У машины изменилась максимальная скорость', maxCarSpeed);
+console.log('У машины изменился владелец', carOwner);
+
+maxCarSpeed = 220;
+carOwner = 'Александр';
+alert(maxCarSpeed);
+alert(carOwner);
+console.log('У машины изменилась максимальная скорость', maxCarSpeed);
+console.log('У машины изменился владелец', carOwner);
---
+++ first_project/Syntax/index.js
@@ -0,0 +1,15 @@
+// Автор
+/*
+Потоцкий Олег
+ */
+let myName = 'Олег';
+let mySurname = 'Потоцкий';
+let myFavoriteDrink = 'Чай';
+let myFavoriteAnimal = 'Кошка';
+let myFavoriteProgrammingLanguage = 'JavaScript';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
