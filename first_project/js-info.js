4fa37ae679ea85a67e2648f7064e5d01871c41ba
oleg086rus <o.pototskiy@yandex.ru>
2022-01-10 13:40:59 +0500

    Модуль 1. База JavaScript Tasks

---
+++ first_project/js-info.js
@@ -0,0 +1,5 @@
+alert("Брендон Айк (Brendon Eich) разработал язык программирования JavaScript");
+alert("за 10 дней создали язык под названием LiveScript, который в том же году с использованием функциональности Java был переименован в JavaScript");
+alert("JavaScript был создан в 1995 году");
+alert("JavaScript - это не Java");
+alert("Спецификация языка, которая управляет JavaScript, называется ECMAScript");
