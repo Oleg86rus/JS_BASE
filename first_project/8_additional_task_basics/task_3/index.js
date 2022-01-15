bde0919b58b9ebcbd7c24017c797b874b07c6780
oleg086rus <o.pototskiy@yandex.ru>
2022-01-15 13:44:25 +0500

    Поправил проверку пароля (добавил наличие строчных букв)

--- first_project/8_additional_task_basics/task_3/index.js
+++ first_project/8_additional_task_basics/task_3/index.js
@@ -3,19 +3,20 @@ let i = 0;
 let symbol = '';
 let numberInPassword = false;
 let lowerCaseInPassword = false;
+let upperCaseInPassword = false;
 if (password.length > 2 && password.length <= 20) {
     while (i < password.length) {
         symbol = password.charAt(i);
         if (!isNaN(symbol)) {
             numberInPassword = true;
+        } else if (symbol === symbol.toUpperCase()) {
+            upperCaseInPassword = true;
         } else {
-            if (symbol === symbol.toUpperCase()) {
-                lowerCaseInPassword = true;
-            }
+            lowerCaseInPassword = true;
         }
         i++;
     }
-    if (numberInPassword === true && lowerCaseInPassword === true) {
+    if (numberInPassword === true && lowerCaseInPassword === true && upperCaseInPassword === true) {
         alert('Пароль валидный. Добро пожаловать в аккаунт!')
     } else {
         alert('Пароль не удовлетворяет условиям');
