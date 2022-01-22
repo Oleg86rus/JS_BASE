7cfc832eafe7778a0bfc21ebba1d8d8861c0f90b
oleg086rus <o.pototskiy@yandex.ru>
2022-01-22 19:44:18 +0500

    Дополнительные задания к урокам 7-9

--- first_project/14_additional_task_array/task_4/index.js
+++ first_project/14_additional_task_array/task_4/index.js
@@ -1,21 +1,21 @@
 /*
-Вам необходимо создать функцию getSumOfSequence, которая будет создавать массив из чисел и высчитывать сумму первого и последнего элементов массива.
+Вам необходимо сделать из элементов вложенных массивов один массив. Итоговый результат должен выглядеть следующим образом: [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ].
 
-Функция принимает в себя один параметр number. Используя этот параметр, нужно создать массив из чисел от 1 до number. Для заполнения массива элементами используйте цикл for. (Например, если был передан number равный 5, то массив должен выглядеть следующим образом: [1, 2, 3, 4, 5])
+Условия:
 
-Функция getSumOfSequence должна возвращать сумму первого и последнего элементов итогового массива.
+Нельзя использовать метод массивов flat.
+Необходимо, чтобы в решении использовался метод concat либо spread-оператор
  */
 
-let numbersArray = [];
-let sumNumbersArray = 0;
-let number = Number(prompt('Введите число для массива').trim());
-const getSumOfSequence = (number) => {
-    for (let i = 1; i <= number; i++) {
-        numbersArray.push(i);
-    }
-    console.log(`Массив: `, numbersArray);
-    sumNumbersArray = numbersArray[0] + numbersArray[numbersArray.length - 1];
-    alert(`Сумма первого и последнего числа массива = ${sumNumbersArray}`);
-    console.log(`Сумма первого и последнего числа массива = ${sumNumbersArray}`);
-};
-getSumOfSequence(number);
+const matrix = [
+    [ 1, 2, 3 ],
+    [ 4, 5, 6 ],
+    [ 7, 8, 9 ],
+];
+console.log(matrix)
+const newMatrix = [
+    ...matrix[0],
+    ...matrix[1],
+    ...matrix[2]
+]
+console.log(newMatrix)
