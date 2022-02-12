6aa8112fdb2e8d709940c21ce68979be6859e655
oleg086rus <o.pototskiy@yandex.ru>
2022-02-12 16:27:28 +0500

    допы DOM

--- first_project/21_additional_DOM/task_1/index.js
+++ first_project/21_additional_DOM/task_1/index.js
@@ -14,39 +14,16 @@
   </ul>
 </div>
  */
-const container = document.querySelector('.container');
 
-const listItems = [
-    {
-        value: 1,
-        text: 'JavaScript'
-    },
-    {
-        value: 2,
-        text: 'NodeJS'
-    },
-    {
-        value: 3,
-        text: 'ReactJS'
-    },
-    {
-        value: 4,
-        text: 'HTML'
-    },
-    {
-        value: 5,
-        text: 'CSS'
-    }
-]
 class CustomSelect {
     constructor(id, options) {
         this.id = id;
         this.options = [...options];
     }
 
-
-
     render (container) {
+        const findNameInOptions = this.options;
+        container = document.querySelector('.container');
         console.log(this.options)
         const selectDropdown = document.createElement('div');
         selectDropdown.className = `select-dropdown select-dropdown--${this.id}`;
@@ -70,25 +47,91 @@ class CustomSelect {
         container.append(selectDropdown);
         selectDropdown.append(selectDropdownButton, selectDropdownList);
         selectDropdownButton.append(selectDropdownSpan);
-        for (let i = 0; i <= this.options.length; i++) {
+        this.options.forEach((el) => {
+            console.log(el)
             let selectDropdownListItem = document.createElement('li');
             selectDropdownListItem.className = 'select-dropdown__list-item';
-            selectDropdownListItem.dataset.value = `${this.options[i].value}`;
-            selectDropdownListItem.textContent = `${this.options[i].text}`;
+            selectDropdownListItem.dataset.value = `${el.value}`;
+            selectDropdownListItem.textContent = `${el.text}`;
             selectDropdownList.append(selectDropdownListItem);
+        })
+        function addEventListenerInUl() {
+            console.log(selectDropdownList)
+            selectDropdownButton.addEventListener('click', () => {
+                const searchClassInUl = [...selectDropdownList.classList];
+                console.log(searchClassInUl)
+                if (!searchClassInUl.includes('active')) {
+                    selectDropdownList.classList.add('active');
+                } else {
+                    selectDropdownList.classList.remove('active');
+                }
+            })
+
         }
+        addEventListenerInUl()
+
+        function addEventListenerInLi() {
+            console.log(selectDropdownListItem)
+            const selectDropdownAllListItems = document.querySelectorAll('.select-dropdown__list-item')
+            // selectDropdownButton.addEventListener('click', (event) => {
+            //     // const { target } = event
+            //     const selectDrodownLi = event.target.closest('.select-dropdown__list-item');
+            //     // console.log(target)
+            //     if (selectDrodownLi) {
+            //         console.log('1')
+            //     }
+            //
+            // })
+
+
+
+        selectDropdownAllListItems.forEach((el) => {
+            el.addEventListener('click', (event) => {
+                console.log(el.dataset.value)
+                const { target } = event;
+                console.log(target.dataset.value, target.textContent)
+                console.log(findNameInOptions)
+                const newTextInButton = findNameInOptions.find((elem) => {
+                    if (elem.value === Number(target.dataset.value)) {
+                        return elem;
+                    }
+                })
+                console.log(newTextInButton)
+                selectDropdownSpan.textContent = newTextInButton.text;
+                selectDropdownList.classList.remove('active');
+            })
+        })
+
+        }
+        addEventListenerInLi()
 
     }
-
-
-
-
-
 }
-
-
-
-
-
+const listItems = [
+    {
+        value: 1,
+        text: 'JavaScript'
+    },
+    {
+        value: 2,
+        text: 'NodeJS'
+    },
+    {
+        value: 3,
+        text: 'ReactJS'
+    },
+    {
+        value: 4,
+        text: 'HTML'
+    },
+    {
+        value: 5,
+        text: 'CSS'
+    }
+]
 const newCustomSelect = new CustomSelect(123, listItems);
-newCustomSelect.render(container)
+newCustomSelect.render();
+
+
+const select2 = new CustomSelect(222, listItems)
+select2.render()
