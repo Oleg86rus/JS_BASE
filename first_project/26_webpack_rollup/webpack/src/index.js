0ca6f8edcea37b14db28caf30d75a2b7ec3e0a0d
oleg086rus <o.pototskiy@yandex.ru>
2022-02-21 21:27:31 +0500

    Webpack

---
+++ first_project/26_webpack_rollup/webpack/src/index.js
@@ -0,0 +1,20 @@
+import '../index.css';
+import JS_IMAGE from '../assets/JS_IMAGE.jpeg';
+
+
+console.log('hello world')
+
+const container = document.createElement('div');
+container.className = 'container';
+
+
+const jsImageHTML = document.createElement('img');
+jsImageHTML.className = 'js-image';
+jsImageHTML.src = JS_IMAGE;
+
+const titleHTML = document.createElement('h1');
+titleHTML.className = 'love-js';
+titleHTML.textContent = 'I love JavaScript';
+
+document.body.append(container);
+container.append(jsImageHTML, titleHTML);
